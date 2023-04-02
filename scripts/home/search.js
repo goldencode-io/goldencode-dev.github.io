967b5b4adaa78baeca350af2bbfb0bdf9321d318
Golden Code <129628595+goldencode-dev@users.noreply.github.com>
2023-04-03 01:16:53 +0430

    Create search.js

---
+++ scripts/home/search.js
@@ -0,0 +1,28 @@
+console.log("start")
+
+function search2(){
+   
+    var search = document.getElementById("searchInput").value;
+
+    
+    document.getElementById("txtSearch").innerHTML = "جستجو : "+search;
+    if (searchInput == "پایتون"){
+
+    }
+    
+
+}
+
+
+function search3(){
+   
+    var search = document.getElementById("searchInput2").value;
+
+    
+    document.getElementById("txtSearch").innerHTML = "جستجو : "+search;
+    if (searchInput == "پایتون"){
+
+    }
+    
+
+}
